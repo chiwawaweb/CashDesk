ee8a8e3f630ed030dc6d508f39e65aa0550ab8bb
chiwawaweb <git@chiwawaweb.com>
2017-12-14 12:03:22 +0100

    Maj

--- CashDesk/MainForm.Designer.cs
+++ CashDesk/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CashDesk
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Variable nécessaire au concepteur.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(844, 581);
+            this.Name = "Form1";
+            this.Text = "CashDesk";
+            this.ResumeLayout(false);
+
         }
 
         #endregion

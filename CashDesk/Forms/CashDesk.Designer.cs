2b3e26304f022801a9fc8ec5581d57d7360050e4
chiwawaweb <git@chiwawaweb.com>
2017-12-14 16:14:54 +0100

    Maj

--- CashDesk/Forms/CashDesk.Designer.cs
+++ CashDesk/Forms/CashDesk.Designer.cs
@@ -40,19 +40,25 @@
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
             this.button10 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgvArticles = new System.Windows.Forms.DataGridView();
             this.button11 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArticles)).BeginInit();
             this.SuspendLayout();
             // 
             // BtnTOne
             // 
             this.BtnTOne.BackColor = System.Drawing.Color.Gold;
             this.BtnTOne.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnTOne.Location = new System.Drawing.Point(683, 46);
+            this.BtnTOne.Location = new System.Drawing.Point(459, 83);
+            this.BtnTOne.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.BtnTOne.Name = "BtnTOne";
-            this.BtnTOne.Size = new System.Drawing.Size(75, 75);
+            this.BtnTOne.Size = new System.Drawing.Size(50, 49);
             this.BtnTOne.TabIndex = 0;
+            this.BtnTOne.TabStop = false;
             this.BtnTOne.Text = "7";
             this.BtnTOne.UseVisualStyleBackColor = false;
             // 
@@ -60,10 +66,12 @@
             // 
             this.button1.BackColor = System.Drawing.Color.Gold;
             this.button1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(683, 347);
+            this.button1.Location = new System.Drawing.Point(459, 242);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(178, 75);
+            this.button1.Size = new System.Drawing.Size(104, 49);
             this.button1.TabIndex = 1;
+            this.button1.TabStop = false;
             this.button1.Text = "0";
             this.button1.UseVisualStyleBackColor = false;
             // 
@@ -71,10 +79,12 @@
             // 
             this.button2.BackColor = System.Drawing.Color.Gold;
             this.button2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(881, 347);
+            this.button2.Location = new System.Drawing.Point(567, 242);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 75);
+            this.button2.Size = new System.Drawing.Size(50, 49);
             this.button2.TabIndex = 2;
+            this.button2.TabStop = false;
             this.button2.Text = ",";
             this.button2.UseVisualStyleBackColor = false;
             // 
@@ -82,10 +92,12 @@
             // 
             this.button3.BackColor = System.Drawing.Color.Gold;
             this.button3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(881, 246);
+            this.button3.Location = new System.Drawing.Point(567, 189);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 75);
+            this.button3.Size = new System.Drawing.Size(50, 49);
             this.button3.TabIndex = 3;
+            this.button3.TabStop = false;
             this.button3.Text = "3";
             this.button3.UseVisualStyleBackColor = false;
             // 
@@ -93,10 +105,12 @@
             // 
             this.button4.BackColor = System.Drawing.Color.Gold;
             this.button4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(683, 246);
+            this.button4.Location = new System.Drawing.Point(459, 189);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 75);
+            this.button4.Size = new System.Drawing.Size(50, 49);
             this.button4.TabIndex = 4;
+            this.button4.TabStop = false;
             this.button4.Text = "1";
             this.button4.UseVisualStyleBackColor = false;
             // 
@@ -104,10 +118,12 @@
             // 
             this.button5.BackColor = System.Drawing.Color.Gold;
             this.button5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(786, 246);
+            this.button5.Location = new System.Drawing.Point(513, 189);
+            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 75);
+            this.button5.Size = new System.Drawing.Size(50, 49);
             this.button5.TabIndex = 5;
+            this.button5.TabStop = false;
             this.button5.Text = "2";
             this.button5.UseVisualStyleBackColor = false;
             // 
@@ -115,10 +131,12 @@
             // 
             this.button6.BackColor = System.Drawing.Color.Gold;
             this.button6.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(683, 146);
+            this.button6.Location = new System.Drawing.Point(459, 136);
+            this.button6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(75, 75);
+            this.button6.Size = new System.Drawing.Size(50, 49);
             this.button6.TabIndex = 6;
+            this.button6.TabStop = false;
             this.button6.Text = "4";
             this.button6.UseVisualStyleBackColor = false;
             // 
@@ -126,10 +144,12 @@
             // 
             this.button7.BackColor = System.Drawing.Color.Gold;
             this.button7.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button7.Location = new System.Drawing.Point(786, 46);
+            this.button7.Location = new System.Drawing.Point(513, 83);
+            this.button7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(75, 75);
+            this.button7.Size = new System.Drawing.Size(50, 49);
             this.button7.TabIndex = 7;
+            this.button7.TabStop = false;
             this.button7.Text = "8";
             this.button7.UseVisualStyleBackColor = false;
             // 
@@ -137,10 +157,12 @@
             // 
             this.button8.BackColor = System.Drawing.Color.Gold;
             this.button8.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button8.Location = new System.Drawing.Point(786, 146);
+            this.button8.Location = new System.Drawing.Point(513, 136);
+            this.button8.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(75, 75);
+            this.button8.Size = new System.Drawing.Size(50, 49);
             this.button8.TabIndex = 8;
+            this.button8.TabStop = false;
             this.button8.Text = "5";
             this.button8.UseVisualStyleBackColor = false;
             // 
@@ -148,10 +170,12 @@
             // 
             this.button9.BackColor = System.Drawing.Color.Gold;
             this.button9.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button9.Location = new System.Drawing.Point(881, 146);
+            this.button9.Location = new System.Drawing.Point(567, 136);
+            this.button9.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(75, 75);
+            this.button9.Size = new System.Drawing.Size(50, 49);
             this.button9.TabIndex = 9;
+            this.button9.TabStop = false;
             this.button9.Text = "6";
             this.button9.UseVisualStyleBackColor = false;
             // 
@@ -159,43 +183,102 @@
             // 
             this.button10.BackColor = System.Drawing.Color.Gold;
             this.button10.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button10.Location = new System.Drawing.Point(881, 46);
+            this.button10.Location = new System.Drawing.Point(567, 83);
+            this.button10.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(75, 75);
+            this.button10.Size = new System.Drawing.Size(50, 49);
             this.button10.TabIndex = 10;
+            this.button10.TabStop = false;
             this.button10.Text = "9";
             this.button10.UseVisualStyleBackColor = false;
             // 
-            // dataGridView1
+            // dgvArticles
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(43, 46);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(510, 463);
-            this.dataGridView1.TabIndex = 11;
+            this.dgvArticles.AllowUserToAddRows = false;
+            this.dgvArticles.AllowUserToDeleteRows = false;
+            this.dgvArticles.BackgroundColor = System.Drawing.Color.White;
+            this.dgvArticles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvArticles.Location = new System.Drawing.Point(32, 83);
+            this.dgvArticles.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgvArticles.Name = "dgvArticles";
+            this.dgvArticles.ReadOnly = true;
+            this.dgvArticles.RowTemplate.Height = 28;
+            this.dgvArticles.Size = new System.Drawing.Size(340, 301);
+            this.dgvArticles.TabIndex = 11;
+            this.dgvArticles.TabStop = false;
             // 
             // button11
             // 
             this.button11.BackColor = System.Drawing.Color.GreenYellow;
             this.button11.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button11.Location = new System.Drawing.Point(683, 453);
+            this.button11.Location = new System.Drawing.Point(459, 295);
+            this.button11.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button11.Name = "button11";
-            this.button11.Size = new System.Drawing.Size(178, 75);
+            this.button11.Size = new System.Drawing.Size(104, 49);
             this.button11.TabIndex = 12;
+            this.button11.TabStop = false;
             this.button11.Text = "Total";
             this.button11.UseVisualStyleBackColor = false;
             // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.Color.LightGreen;
+            this.button12.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button12.Location = new System.Drawing.Point(376, 83);
+            this.button12.Margin = new System.Windows.Forms.Padding(2);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(32, 31);
+            this.button12.TabIndex = 13;
+            this.button12.TabStop = false;
+            this.button12.Text = "-";
+            this.button12.UseVisualStyleBackColor = false;
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.Color.LightGreen;
+            this.button13.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button13.Location = new System.Drawing.Point(376, 353);
+            this.button13.Margin = new System.Windows.Forms.Padding(2);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(32, 31);
+            this.button13.TabIndex = 14;
+            this.button13.TabStop = false;
+            this.button13.Text = "-";
+            this.button13.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.GreenYellow;
+            this.textBox1.Location = new System.Drawing.Point(32, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(585, 39);
+            this.textBox1.TabIndex = 15;
+            this.textBox1.TabStop = false;
+            this.textBox1.Text = "0.00";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(32, 389);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(340, 20);
+            this.textBox2.TabIndex = 16;
+            // 
             // CashDesk
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(995, 743);
+            this.ClientSize = new System.Drawing.Size(651, 483);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
             this.Controls.Add(this.button11);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dgvArticles);
             this.Controls.Add(this.button10);
             this.Controls.Add(this.button9);
             this.Controls.Add(this.button8);
@@ -209,12 +292,14 @@
             this.Controls.Add(this.BtnTOne);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "CashDesk";
             this.Text = "CashDesk";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArticles)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -231,7 +316,11 @@
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button9;
         private System.Windows.Forms.Button button10;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgvArticles;
         private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
